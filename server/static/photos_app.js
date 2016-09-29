4c906777a37c1f058734e4b7060444857dec865e
Bo <bchean@gmail.com>
2016-09-29 04:59:59 -0700

    Impl photo selection and key shortcuts.

--- server/static/photos_app.js
+++ server/static/photos_app.js
@@ -1,11 +1,13 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 var Backbone = require('backbone'),
-    _ = require('underscore');
+    _ = require('underscore'),
+    $ = require('jquery');
 
 var dispatcher = _.extend({}, Backbone.Events);
 
 var PhotoModel = Backbone.Model.extend({
   defaults: {
+    id: null,
     filename: null,
     description: null,
     date: null
@@ -17,12 +19,27 @@ var PhotoCollection = Backbone.Collection.extend({
   model: PhotoModel
 });
 
+var LabelModel = Backbone.Model.extend({
+  defaults: {
+    id: null,
+    name: null,
+    color: null
+  }
+});
+
+var LabelCollection = Backbone.Collection.extend({
+  url: '/api/labels',
+  model: LabelModel
+});
+
 var PhotoListItemView = Backbone.View.extend({
   tagName: 'li',
   className: 'photoListItem',
 
+  isSelected: false,
+
   events: {
-    click: 'fireUpdateCurrentPhoto'
+    'click': 'fireClickPhoto'
   },
 
   render: function() {
@@ -34,16 +51,20 @@ var PhotoListItemView = Backbone.View.extend({
     return this;
   },
 
-  fireUpdateCurrentPhoto: function() {
-    dispatcher.trigger('update:currentPhoto', this.model);
+  fireClickPhoto: function() {
+    dispatcher.trigger('click:photo', this.$el);
   }
 })
 
 var PhotoListView = Backbone.View.extend({
   el: '#photoList',
 
+  // 1-based. We start at a dummy value for convenience.
+  selectedIndex_1: null,
+
   initialize: function() {
     this.listenTo(this.collection, 'sync', this.render);
+    dispatcher.on('click:photo', this.handleClickPhoto, this);
   },
 
   render: function() {
@@ -52,6 +73,64 @@ var PhotoListView = Backbone.View.extend({
       this.$el.append(photoListItem.render().$el);
     }, this);
     return this;
+  },
+
+  handleClickPhoto: function($clickedPhoto) {
+    var clickedIndex_0 = this.$el.children().index($clickedPhoto);
+    console.log('clicked ' + clickedIndex_0);
+    this.selectPhoto(clickedIndex_0);
+  },
+
+  selectPreviousPhoto: function() {
+    var oldIndex_0 = this.selectedIndex_1 - 1;
+    var newIndex_0 = oldIndex_0 - 1;
+    this.selectPhoto(newIndex_0);
+  },
+
+  selectNextPhoto: function() {
+    var oldIndex_0 = this.selectedIndex_1 - 1;
+    var newIndex_0 = oldIndex_0 + 1;
+    this.selectPhoto(newIndex_0);
+  },
+
+  selectFirstPhoto: function() {
+    this.selectPhoto(0);
+  },
+
+  selectLastPhoto: function() {
+    this.selectPhoto(this.collection.length-1);
+  },
+
+  selectPhoto: function(photoIndex_0) {
+    if (!this.collection.length) {
+      return;
+    }
+
+    // This will be false the first time this function is called (initial dummy value = 0).
+    if (this.selectedIndex_1 >= 1) {
+      this.$(':nth-child(' + this.selectedIndex_1 + ')').removeClass('selected');
+    }
+
+    photoIndex_0 = (photoIndex_0 + this.collection.length) % this.collection.length;
+    var photoIndex_1 = photoIndex_0 + 1;
+    var newPhotoIsHigher = (photoIndex_1 < this.selectedIndex_1);
+
+    var $selectedPhoto = this.$(':nth-child(' + photoIndex_1 + ')');
+    $selectedPhoto.addClass('selected');
+    this.scrollIntoViewIfNecessary($selectedPhoto[0], newPhotoIsHigher);
+
+    this.selectedIndex_1 = photoIndex_1;
+
+    dispatcher.trigger('update:currentPhoto', this.collection.at(photoIndex_0));
+  },
+
+  scrollIntoViewIfNecessary: function(el, scrollingUp) {
+    var elTop = el.getBoundingClientRect().top;
+    var elBottom = el.getBoundingClientRect().bottom;
+    var elIsInView = (elTop >= 0) && (elBottom <= window.innerHeight);
+    if (!elIsInView) {
+      el.scrollIntoView(scrollingUp);
+    }
   }
 })
 
@@ -60,12 +139,6 @@ var CurrentPhotoView = Backbone.View.extend({
 
   initialize: function() {
     dispatcher.on('update:currentPhoto', this.handleUpdateCurrentPhoto, this);
-
-    this.listenToOnce(this.collection, 'sync', function() {
-      if (this.collection.length) {
-        this.handleUpdateCurrentPhoto(this.collection.at(0));
-      }
-    });
   },
 
   render: function() {
@@ -82,12 +155,57 @@ var CurrentPhotoView = Backbone.View.extend({
   }
 })
 
+var CurrentPhotoLabelsView = Backbone.View.extend({
+  el: '#currentPhotoContainer #labels',
+
+  initialize: function() {
+    dispatcher.on('update:currentPhoto', this.handleUpdateCurrentPhoto, this);
+    this.listenTo(this.collection, 'sync', this.render);
+  },
+
+  render: function() {
+    var labels = this.collection.map(function(labelModel) {
+      return labelModel.get('name');
+    });
+    var labelStr = labels.join(', ');
+    this.$el.html(labelStr || 'no labels yet');
+    return this;
+  },
+
+  handleUpdateCurrentPhoto: function(newPhotoModel) {
+    var newPhotoId = newPhotoModel.get('id');
+    this.collection.fetch({data: {photo_id: newPhotoId}});
+  }
+})
+
 var photoCollection = new PhotoCollection();
 var photoListView = new PhotoListView({collection: photoCollection});
 var currentPhotoView = new CurrentPhotoView({collection: photoCollection});
+
+var labelCollection = new LabelCollection();
+var currentPhotoLabelsView = new CurrentPhotoLabelsView({collection: labelCollection});
+
+dispatcher.listenToOnce(photoCollection, 'sync', function() {
+  if (photoCollection.length) {
+    photoListView.selectFirstPhoto();
+  }
+});
+
+document.onkeypress = function(e) {
+  if (e.key === 'j') {
+    photoListView.selectNextPhoto();
+  } else if (e.key === 'k') {
+    photoListView.selectPreviousPhoto();
+  } else if (e.key === 'g') {
+    photoListView.selectFirstPhoto();
+  } else if (e.key === 'G') {
+    photoListView.selectLastPhoto();
+  }
+};
+
 photoCollection.fetch();
 
-},{"backbone":2,"underscore":4}],2:[function(require,module,exports){
+},{"backbone":2,"jquery":3,"underscore":4}],2:[function(require,module,exports){
 (function (global){
 //     Backbone.js 1.3.3
 
--- ui-dev/apps/photos.js
+++ ui-dev/apps/photos.js
@@ -1,10 +1,12 @@
 var Backbone = require('backbone'),
-    _ = require('underscore');
+    _ = require('underscore'),
+    $ = require('jquery');
 
 var dispatcher = _.extend({}, Backbone.Events);
 
 var PhotoModel = Backbone.Model.extend({
   defaults: {
+    id: null,
     filename: null,
     description: null,
     date: null
@@ -16,12 +18,27 @@ var PhotoCollection = Backbone.Collection.extend({
   model: PhotoModel
 });
 
+var LabelModel = Backbone.Model.extend({
+  defaults: {
+    id: null,
+    name: null,
+    color: null
+  }
+});
+
+var LabelCollection = Backbone.Collection.extend({
+  url: '/api/labels',
+  model: LabelModel
+});
+
 var PhotoListItemView = Backbone.View.extend({
   tagName: 'li',
   className: 'photoListItem',
 
+  isSelected: false,
+
   events: {
-    click: 'fireUpdateCurrentPhoto'
+    'click': 'fireClickPhoto'
   },
 
   render: function() {
@@ -33,16 +50,20 @@ var PhotoListItemView = Backbone.View.extend({
     return this;
   },
 
-  fireUpdateCurrentPhoto: function() {
-    dispatcher.trigger('update:currentPhoto', this.model);
+  fireClickPhoto: function() {
+    dispatcher.trigger('click:photo', this.$el);
   }
 })
 
 var PhotoListView = Backbone.View.extend({
   el: '#photoList',
 
+  // 1-based. We start at a dummy value for convenience.
+  selectedIndex_1: null,
+
   initialize: function() {
     this.listenTo(this.collection, 'sync', this.render);
+    dispatcher.on('click:photo', this.handleClickPhoto, this);
   },
 
   render: function() {
@@ -51,6 +72,64 @@ var PhotoListView = Backbone.View.extend({
       this.$el.append(photoListItem.render().$el);
     }, this);
     return this;
+  },
+
+  handleClickPhoto: function($clickedPhoto) {
+    var clickedIndex_0 = this.$el.children().index($clickedPhoto);
+    console.log('clicked ' + clickedIndex_0);
+    this.selectPhoto(clickedIndex_0);
+  },
+
+  selectPreviousPhoto: function() {
+    var oldIndex_0 = this.selectedIndex_1 - 1;
+    var newIndex_0 = oldIndex_0 - 1;
+    this.selectPhoto(newIndex_0);
+  },
+
+  selectNextPhoto: function() {
+    var oldIndex_0 = this.selectedIndex_1 - 1;
+    var newIndex_0 = oldIndex_0 + 1;
+    this.selectPhoto(newIndex_0);
+  },
+
+  selectFirstPhoto: function() {
+    this.selectPhoto(0);
+  },
+
+  selectLastPhoto: function() {
+    this.selectPhoto(this.collection.length-1);
+  },
+
+  selectPhoto: function(photoIndex_0) {
+    if (!this.collection.length) {
+      return;
+    }
+
+    // This will be false the first time this function is called (initial dummy value = 0).
+    if (this.selectedIndex_1 >= 1) {
+      this.$(':nth-child(' + this.selectedIndex_1 + ')').removeClass('selected');
+    }
+
+    photoIndex_0 = (photoIndex_0 + this.collection.length) % this.collection.length;
+    var photoIndex_1 = photoIndex_0 + 1;
+    var newPhotoIsHigher = (photoIndex_1 < this.selectedIndex_1);
+
+    var $selectedPhoto = this.$(':nth-child(' + photoIndex_1 + ')');
+    $selectedPhoto.addClass('selected');
+    this.scrollIntoViewIfNecessary($selectedPhoto[0], newPhotoIsHigher);
+
+    this.selectedIndex_1 = photoIndex_1;
+
+    dispatcher.trigger('update:currentPhoto', this.collection.at(photoIndex_0));
+  },
+
+  scrollIntoViewIfNecessary: function(el, scrollingUp) {
+    var elTop = el.getBoundingClientRect().top;
+    var elBottom = el.getBoundingClientRect().bottom;
+    var elIsInView = (elTop >= 0) && (elBottom <= window.innerHeight);
+    if (!elIsInView) {
+      el.scrollIntoView(scrollingUp);
+    }
   }
 })
 
@@ -59,12 +138,6 @@ var CurrentPhotoView = Backbone.View.extend({
 
   initialize: function() {
     dispatcher.on('update:currentPhoto', this.handleUpdateCurrentPhoto, this);
-
-    this.listenToOnce(this.collection, 'sync', function() {
-      if (this.collection.length) {
-        this.handleUpdateCurrentPhoto(this.collection.at(0));
-      }
-    });
   },
 
   render: function() {
@@ -81,7 +154,52 @@ var CurrentPhotoView = Backbone.View.extend({
   }
 })
 
+var CurrentPhotoLabelsView = Backbone.View.extend({
+  el: '#currentPhotoContainer #labels',
+
+  initialize: function() {
+    dispatcher.on('update:currentPhoto', this.handleUpdateCurrentPhoto, this);
+    this.listenTo(this.collection, 'sync', this.render);
+  },
+
+  render: function() {
+    var labels = this.collection.map(function(labelModel) {
+      return labelModel.get('name');
+    });
+    var labelStr = labels.join(', ');
+    this.$el.html(labelStr || 'no labels yet');
+    return this;
+  },
+
+  handleUpdateCurrentPhoto: function(newPhotoModel) {
+    var newPhotoId = newPhotoModel.get('id');
+    this.collection.fetch({data: {photo_id: newPhotoId}});
+  }
+})
+
 var photoCollection = new PhotoCollection();
 var photoListView = new PhotoListView({collection: photoCollection});
 var currentPhotoView = new CurrentPhotoView({collection: photoCollection});
+
+var labelCollection = new LabelCollection();
+var currentPhotoLabelsView = new CurrentPhotoLabelsView({collection: labelCollection});
+
+dispatcher.listenToOnce(photoCollection, 'sync', function() {
+  if (photoCollection.length) {
+    photoListView.selectFirstPhoto();
+  }
+});
+
+document.onkeypress = function(e) {
+  if (e.key === 'j') {
+    photoListView.selectNextPhoto();
+  } else if (e.key === 'k') {
+    photoListView.selectPreviousPhoto();
+  } else if (e.key === 'g') {
+    photoListView.selectFirstPhoto();
+  } else if (e.key === 'G') {
+    photoListView.selectLastPhoto();
+  }
+};
+
 photoCollection.fetch();
